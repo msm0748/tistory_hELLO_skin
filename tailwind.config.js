8592b3c23f5c48206f842bad5330a58e302430e1
seokmin <msm0748@gmail.com>
2023-07-23 09:27:31 +0900

    design: 사이드바 및 메인 반응형  스타일 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,54 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app.pug', './index.pug', './views/**/*.pug'],
-  darkMode: 'class',
+  content: ["./app.pug", "./index.pug", "./views/**/*.pug"],
+  darkMode: "class",
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1100px',
-      xl: '1400px',
-      '2xl': '1820px'
+      sm: "640px",
+      md: "768px",
+      lg: "1140px",
+      xl: "1460px",
+      "2xl": "1820px",
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       h: {
-        100: '#ffffff',
-        200: '#f4f4f6',
-        300: '#e6e6e9',
-        400: '#9999a1',
-        500: '#66666e',
-        600: '#353638',
-        700: '#292a2d',
-        800: '#1e1f21',
-        900: '#000000',
-        blue: '#5db0d7',
-        orange: '#ff5544'
-      }
+        100: "#ffffff",
+        200: "#f4f4f6",
+        300: "#e6e6e9",
+        400: "#9999a1",
+        500: "#66666e",
+        600: "#353638",
+        700: "#292a2d",
+        800: "#1e1f21",
+        900: "#000000",
+        blue: "#5db0d7",
+        orange: "#ff5544",
+      },
     },
     extend: {
       width: {
-        idx: '1100px',
-        pem: 'var(--h-width)',
-        s: '16rem', // sidebar
-        c: 'calc(100% - 16rem)' // 100% - width.sidebar
+        idx: "1100px",
+        pem: "var(--h-width)",
+        s: "20rem", // sidebar
+        c: "calc(100% - 20rem)", // 100% - width.sidebar
       },
       keyframes: {
         loading: {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg)' }
-        }
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
       animation: {
-        loading: 'loading 1s linear infinite'
+        loading: "loading 1s linear infinite",
       },
       fontFamily: {
-        sans: [
-          'Noto Sans KR',
-          ...defaultTheme.fontFamily.sans
-        ],
-        mono: [
-          'D2Coding',
-          ...defaultTheme.fontFamily.mono
-        ]
-      }
-    }
-  }
-}
+        sans: ["Noto Sans KR", ...defaultTheme.fontFamily.sans],
+        mono: ["D2Coding", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+};
